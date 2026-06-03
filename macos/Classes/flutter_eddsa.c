e86845f429e2af34372232b7af375ebb3c4a5cb6
Mahyar Moghimi <mahyar.moqimi@gmail.com>
2026-06-03 14:48:10 +0200

    chore: add platform build scaffolding (android, ios, macos, windows)

---
+++ macos/Classes/flutter_eddsa.c
@@ -0,0 +1,9 @@
+// Aggregator: includes all crypto source files for macOS static linking.
+#include "../../src/crypto/fld.c"
+#include "../../src/crypto/sc.c"
+#include "../../src/crypto/ed.c"
+#include "../../src/crypto/sha512.c"
+#include "../../src/crypto/ed25519-sha512.c"
+#include "../../src/crypto/x25519.c"
+#include "../../src/crypto/burn.c"
+#include "../../src/crypto/burnstack.c"
